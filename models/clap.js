e55785c9bac21ef8f909ce9bf19ed4fa3367ac07
The-Vampiire <vampiirecodes@gmail.com>
2018-12-21 00:05:00 -0500

    Clap: rename collections to be plural

--- models/clap.js
+++ models/clap.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const clapSchema = new mongoose.Schema({
   count: Number,
-  user: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' },
-  story: { type: mongoose.SchemaTypes.ObjectId, ref: 'Story' },
+  user: { type: mongoose.SchemaTypes.ObjectId, ref: 'users' },
+  story: { type: mongoose.SchemaTypes.ObjectId, ref: 'stories' },
 });
 
-const Clap = mongoose.model('Clap', clapSchema);
+const Clap = mongoose.model('claps', clapSchema);
 
 module.exports = Clap;
