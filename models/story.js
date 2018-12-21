33683f24cff8eea2a1f0c7b3455d0d4d2f8a3388
The-Vampiire <vampiirecodes@gmail.com>
2018-12-21 00:05:13 -0500

    Story: add virtuals, rename collections

--- models/story.js
+++ models/story.js
@@ -3,16 +3,37 @@ const mongoose = require('mongoose');
 const storySchema = new mongoose.Schema({
   title: String,
   body: String,
-  clapCount: {
-    type: Number,
-    default: 0,
-  },
-  author: { type: mongoose.SchemaTypes.ObjectId, ref: 'User' },
-  parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'Story' },
-  replies: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Story' }],
-  claps: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Clap' }],
+  author: { type: mongoose.SchemaTypes.ObjectId, ref: 'users' },
+  parent: { type: mongoose.SchemaTypes.ObjectId, ref: 'stories' },
 });
 
-const Story = mongoose.model('Story', storySchema);
+// https://mongoosejs.com/docs/api.html#schema_Schema-virtual
+storySchema.virtual('claps', {
+  ref: 'claps', // collection name this [claps] field references
+  localField: '_id', // the ID to of this story
+  foreignField: 'story', // the field on the Clap document to match with the ID
+});
+
+storySchema.virtual('clapsCount', {
+  ref: 'claps', // collection name this [claps] field references
+  localField: '_id', // the ID to of this story
+  foreignField: 'story', // the field on the Clap document to match with the ID
+  count: true, // only return a the total number of claps not the Clap documents themselves
+});
+
+storySchema.virtual('replies', {
+  ref: 'stories', // collection name this [Story] field references
+  localField: '_id', // the ID to of this story
+  foreignField: 'parent', // the field on the Clap document to match with the ID
+});
+
+storySchema.virtual('repliesCount', {
+  ref: 'stories', // collection name this [Story] field references
+  localField: '_id', // the ID to of this story
+  foreignField: 'replies', // the field on the Reply [Story] document to match with the ID
+  count: true, // only return a the total number of Replies not the Reply documents themselves
+});
+
+const Story = mongoose.model('stories', storySchema);
 
 module.exports = Story;
