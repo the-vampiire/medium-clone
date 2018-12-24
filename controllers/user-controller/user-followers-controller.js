1169ecd3bbdc56d239df7f0d83828ffa345da30b
The-Vampiire <vampiirecodes@gmail.com>
2018-12-23 20:08:10 -0500

    UserFollowersCont: add notes

--- controllers/user-controller/user-followers-controller.js
+++ controllers/user-controller/user-followers-controller.js
@@ -1,10 +1,15 @@
 const express = require('express');
+// all route handlers that requireAuthedUser will have this middleware executed first
+// if there is no authenticated user then the request will be rejected before reaching
+// the route handler
 const { requireAuthedUser } = require('../../middleware');
 
 const UserFollowersController = express.Router();
 
 UserFollowersController.get((req, res) => {});
+
 UserFollowersController.post(requireAuthedUser, (req, res) => {});
+
 UserFollowersController.delete(requireAuthedUser, (req, res) => {});
 
 module.exports = UserFollowersController;
