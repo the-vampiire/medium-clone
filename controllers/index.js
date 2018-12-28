3ebe30e7a9574ab3f0ff04dcb618b8beb51baed3
The-Vampiire <vampiirecodes@gmail.com>
2018-12-27 22:19:47 -0500

    controllers/ : make directories for story and auth controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,56 +1,5 @@
-const authenticationCookie = {};
+const UserController = require('./user-controller');
 
-const endpointMap = {
-  user: {
-    prefix: ':username',
-    followers: [
-
-    ],
-    following: {
-
-    },
-    stories: [
-
-    ],
-    responses: {
-
-    },
-    claps: {
-
-    },
-  },
-
-  story: {
-
-  },
-
-  topics: {
-    root: {
-
-    },
-
-  },
-
-  auth: {
-    register: {
-      method: 'POST',
-      example: {
-        username: 'the-vampiire',
-        password: 'password',
-        confirmPassword: 'password',
-      },
-    },
-    login: {
-      method: 'POST',
-      authorization: null,
-      example: {
-        username: 'the-vampiire',
-        password: 'password',
-      },
-    },
-    logout: {
-      method: 'GET',
-      authentication: authenticationCookie,
-    },
-  }
+module.exports = {
+  UserController,
 };
