7866c1c3d92e3bc17403e656afe502dd59c407a1
The-Vampiire <vampiirecodes@gmail.com>
2019-01-27 00:49:04 -0500

    /users: ref to use UserController for /users/@username/ routes

--- controllers/user-controller/index.js
+++ controllers/user-controller/index.js
@@ -1,24 +1,12 @@
 const express = require('express');
-
-const UserFollowController = require('./user-follow-controller');
+const UserController = require('./user-controller');
 const UserControllerMiddleware = require('./user-controller-middleware');
-const routeHandlers = require('./user-route-handlers');
 
-// handles all requests prefixed by: /user/
-const UserController = express.Router();
+// controls: /users
+const UsersController = express.Router();
 
-// handles the GET/POST/DELETE routes of /user/@username/follow/
-// GET: no content, 200 if following, 404 if not
-UserController.use('/follow', UserFollowController);
+// UsersController.post('/', registerUser);
+UsersController.use('/:usernameSlug', ...UserControllerMiddleware, UserController);
 
-// UserController.get('/', userDiscoveryHandler);
-UserController.get('/stories', routeHandlers.stories);
-UserController.get('/responses', routeHandlers.responses);
-UserController.get('/following', routeHandlers.following);
-UserController.get('/clapped', routeHandlers.clappedStories);
-// UserController.get('/followers', userFollowersHandler);
+module.exports = UsersController;
 
-module.exports = {
-  UserController,
-  UserControllerMiddleware,
-};
