7b8bb8c38516a0f0bc6517c1dd61acbff38f0658
The-Vampiire <vampiirecodes@gmail.com>
2019-02-01 14:41:43 -0500

    Users Controller: registration endpoint, :usernameSlug controller

--- controllers/users-controller/index.js
+++ controllers/users-controller/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const UserController = require('./user-controller');
-const UserControllerMiddleware = require('./user-controller-middleware');
+const { verifyPayload, checkDuplicate, registerUser } = require('./user-registration');
+const { exchangeSlugForUser, userNotFoundRedirect } = require('./user-controller-middleware');
 
-// controls: /users
 const UsersController = express.Router();
 
-// UsersController.post('/', registerUser);
-UsersController.use('/:usernameSlug', ...UserControllerMiddleware, UserController);
+// POST /users (registration)
+UsersController.post(
+  '/',
+  verifyPayload,
+  checkDuplicate,
+  registerUser,
+);
+
+// /users/:usernameSlug MW and Controller
+UsersController.use(
+  '/:usernameSlug',
+  exchangeSlugForUser,
+  userNotFoundRedirect,
+  UserController,
+);
 
 module.exports = UsersController;
 
