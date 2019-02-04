67cced7154d043542af0b1db868934a1a334b938
The-Vampiire <vampiirecodes@gmail.com>
2019-02-04 17:49:14 -0500

    App: add /stories controller import

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const UsersController = require('./users-controller');
 const TokensController = require('./tokens-controller');
+const StoriesController = require('./stories-controller');
 
 module.exports = {
   UsersController,
   TokensController,
+  StoriesController,
 };
