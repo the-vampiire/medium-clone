c792a9d3e1876cb24a665dc843c58b65ff5f1a0a
The-Vampiire <vampiirecodes@gmail.com>
2019-02-06 16:13:44 -0500

    Story static queries: completed getLatestStories() method and tests

--- models/story/story-static-methods/story-static-queries.js
+++ models/story/story-static-methods/story-static-queries.js
@@ -1,18 +1,28 @@
-async function getLatestStories(query) {
-  const { limit = 10, currentPage = 0 } = query;
+/**
+ * Gets the latest published stories
+ * - uses limit, currentPage pagination
+ * - sorted by descending order of publish date
+ * @param {object} paginationQuery pagination query string params
+ * @param {number} paginationQuery.limit [10] pagination limit
+ * @param {number} paginationQuery.currentPage [0] pagination current page
+ * @returns {object} { stories, pagination } paginated output using Story.addPagination
+ */
+async function getLatestStories(paginationQuery) {
+  const { limit = 10, currentPage = 0 } = paginationQuery;
 
   // limit to max of 20 results per page
-  const limitBy = Math.max(limit, 20);
+  const limitBy = Math.min(limit, 20);
   const skipBy = limitBy * currentPage;
 
-  const latestStories = await models.Story
-    .find({ published: true })
-    .sort({ publishedAt: -1 })
+  const latestStories = await this
+    .find({ published: true, parent: null }) // only published stories
+    .sort({ publishedAt: -1 }) // publish date descending
     .limit(limitBy)
     .skip(skipBy);
 
   const stories = await Promise.all(latestStories.map(story => story.toResponseShape()));
-  return { stories, pagination };
+
+  return this.addPagination({ output: { stories }, limit: limitBy, currentPage });
 }
 
 module.exports = {
