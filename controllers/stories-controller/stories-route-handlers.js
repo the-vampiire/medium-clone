7a4a0066e8d7c32376c306787863aec991b8cc1b
The-Vampiire <vampiirecodes@gmail.com>
2019-02-06 16:46:18 -0500

    /stories handlers: added JSDoc to newStory and getLatestStories handlers

--- controllers/stories-controller/stories-route-handlers.js
+++ controllers/stories-controller/stories-route-handlers.js
@@ -1,3 +1,12 @@
+/**
+ * Creates and shapes the response of a new Story
+ * @requires req.authedUser: an authenticated User as the author
+ * @requires req.models: Database models
+ * @param {Request} req Request object
+ * @param {Response} res Response object
+ * @returns {error} 400 JSON response if the title is missing { error }
+ * @returns {Story} JSON response with the newly created Story in Story Response shape
+ */
 const newStoryHandler = async (req, res) => {
   const { body: { title, body }, authedUser, models } = req;
 
@@ -9,6 +18,15 @@ const newStoryHandler = async (req, res) => {
   return res.json(responseShape);
 };
 
+/**
+ * Retrieves a paginable list of recently published stories
+ * @requires req.models: Database models
+ * @param {Request} req Request object
+ * @param {number} req.query.limit pagination limit
+ * @param {number} req.query.currentPage pagination current page
+ * @param {Response} res Response object
+ * @returns {object} JSON response with { stories, pagination } shape
+ */
 const latestStoriesHandler = async (req, res) => {
   const { query, models } = req;
   return res.json(models.Story.getLatestStories(query));
