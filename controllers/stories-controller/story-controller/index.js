6e75093aa6eb52f517a172d23ab75d58d3a9246f
The-Vampiire <vampiirecodes@gmail.com>
2019-02-07 14:40:32 -0500

    Story Controller: GET/PUT/POST handlers and middleware imported

---
+++ controllers/stories-controller/story-controller/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const { requireAuthedUser } = require('../../require-authed-user'); 
+const { exchangeSlugForStory, requireAuthorship } = require('./story-controller-middleware');
+const {
+  storyDiscoveryHandler,
+  storyUpdateHandler,
+  storyDeleteHandler,
+} = require('./story-controller-handlers');
+
+// Story Controller: /stories/:storySlug/
+const StoryController = express.Router();
+
+StoryController.get('/', storyDiscoveryHandler);
+StoryController.put('/', requireAuthedUser, requireAuthorship, storyUpdateHandler);
+StoryController.delete('/', requireAuthedUser, requireAuthorship, storyDeleteHandler);
+
+
+  // ResponsesController: /stories/:storySlug/responses
+  // GET: story responses
+  // POST: respond to story
+  // DELETE: 
+
+  // ClapsController: /stories/:storySlug/claps
+  // GET: { clappedUser: [], readerClaps: Number }
+  // POST: clap for story
+  // PUT: update claps on clapped story
+  // DELETE: remove claps on story
+
+module.exports = {
+  exchangeSlugForStory,
+  StoryController,
+};
