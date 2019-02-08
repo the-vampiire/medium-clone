e940aabf012df879f070110a909dc2154ee36712
The-Vampiire <vampiirecodes@gmail.com>
2019-02-07 23:43:40 -0500

    App: clean up startup, use external defined MW

--- app.js
+++ app.js
@@ -1,47 +1,35 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const models = require('./models');
+const controllers = require('./controllers');
 const { getDatabaseURI } = require('./utils');
-const { UsersController, TokensController, StoriesController } = require('./controllers');
+const appMiddleware = require('./app-middleware');
 
 const app = express();
+const { PORT, DOMAIN, NODE_ENV } = process.env;
 
 // -- SERVERWIDE MIDDLEWARE -- //
-// TODO: export from app middleware
-// add errorHandler to req object
-app.use(
-  bodyParser.json(),
-  bodyParser.urlencoded({ extended: false }),
-  (req, _, next) => {
-    // add db models to all request objects for easy access and testing
-    req.models = models;
-    next();
-  },
-);
+app.use(...appMiddleware);
 
 // -- ENDPOINTS -- //
-app.use('/users', UsersController);
-app.use('/tokens', TokensController);
-app.use('/stories', StoriesController);
+app.use('/users', controllers.UsersController);
+app.use('/tokens', controllers.TokensController);
+app.use('/stories', controllers.StoriesController);
 
 // -- DATABASE INIT -- //
 mongoose.set('useCreateIndex', true);
 mongoose.set('useNewUrlParser', true);
 
 mongoose.connect(
-  getDatabaseURI(process.env.NODE_ENV),
+  getDatabaseURI(NODE_ENV),
   (err) => console.log(err || 'connected to mongo'),
 );
 
 // -- SERVER INIT -- //
-const DOMAIN = process.env.DOMAIN;
-const PORT = process.env.PORT;
-
 app.listen(
   PORT,
   (err) => console.log(err || `listening on ${DOMAIN}`),
 );
 
+module.exports = app;
