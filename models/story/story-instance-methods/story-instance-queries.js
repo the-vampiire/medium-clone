c8200dc458c89bd57c776a5d1e15a5f091df07a1
The-Vampiire <vampiirecodes@gmail.com>
2019-02-08 22:49:47 -0500

    Story instance queries: getReplies() and tests

--- models/story/story-instance-methods/story-instance-queries.js
+++ models/story/story-instance-methods/story-instance-queries.js
@@ -1,9 +1,15 @@
+/**
+ * Reduces the total claps count across all User claps
+ * @returns {number} the total clap count for the Story
+ */
 async function getClapsCount() {
   const reduceClapsCount = claps => claps.reduce((total, clap) => total + clap.count, 0);
 
   return this.populate('claps').execPopulate().then(() => reduceClapsCount(this.claps));
 }
 
+// todo: refactor or remove
+// too heavy unless paginated, what is the use case?
 async function getClappedReaders() {
   const mapClappedUsers = claps => Promise.all(claps.map(
     clap => clap.populate('user').execPopulate().then(populated => populated.user),
@@ -12,7 +18,36 @@ async function getClappedReaders() {
   return this.populate('claps').execPopulate().then(() => mapClappedUsers(this.claps));
 }
 
+/**
+ * Gets a paginated list of of the Story replies
+ * - uses default { limit: 10, currentPage: 0 } if either are missing
+ * @param {number} queryParams.limit pagination limit
+ * @param {number} queryParams.currentPage pagination current page
+ * @returns a paginated replies list { replies, pagination }
+ */
+async function getReplies(queryParams) {
+  const { limit = 10, currentPage = 0 } = queryParams;
+
+  const populated = await this.populate({
+    path: 'replies',
+    options: {
+      limit,
+      skip: currentPage * limit,
+      sort: { publishedAt: -1 }, // createdAt descending
+      match: { published: true }, // only published
+    },
+  }).execPopulate();
+
+  const replies = await Promise.all(
+    populated.replies.map(reply => reply.toResponseShape()),
+  );
+
+  // static method access
+  return this.model('Story').addPagination({ output: { replies }, limit, currentPage });
+}
+
 module.exports = {
   getClapsCount,
   getClappedReaders,
+  getReplies,
 };
--- models/story/story-instance-methods/tests/story-instance-queries.test.js
+++ models/story/story-instance-methods/tests/story-instance-queries.test.js
@@ -1,4 +1,4 @@
-const { getClapsCount, getClappedReaders } = require('../story-instance-queries');
+const { getClapsCount, getClappedReaders, getReplies } = require('../story-instance-queries');
 
 const populaterMock = {
   populate() { return this; },
@@ -29,5 +29,46 @@ describe('Story instance query methods', () => {
     const output = await storyMock.getClappedReaders();
     expect(output.length).toBe(numClaps);
     expect(output[0].id).toBe(0); // IDs are set by index when mapping the clap mocks
+  });
+
+  describe('getReplies(): gets a paginated list of the story replies', () => {
+    const replyMock = { toResponseShape: jest.fn(() => replyMock) };
+    const replies = Array(5).fill().map(() => replyMock);
+    const limit = 3;
+    const currentPage = 5;
+    const expectedPopulateOptions = {
+      path: 'replies',
+      options: {
+        limit,
+        skip: currentPage * limit,
+        sort: { publishedAt: -1 },
+        match: { published: true },
+      },
+    };
+
+    const StoryMock = { addPagination: jest.fn() };
+
+    const storyMock = {
+      replies,
+      getReplies,
+      populate: jest.fn(() => storyMock),
+      execPopulate: jest.fn(() => storyMock),
+      model: jest.fn(() => StoryMock), // simulate Story model access
+    };
+
+    beforeAll(() => storyMock.getReplies({ limit, currentPage }));
+
+    test('populates the replies: uses pagination, publishedAt desc sorting, and only published', () => {
+      expect(storyMock.populate).toHaveBeenCalledWith(expectedPopulateOptions);
+      expect(storyMock.execPopulate).toHaveBeenCalled();
+    });
+
+    test('calls toResponseShape() on each reply found', () => {
+      expect(replies[0].toResponseShape).toHaveBeenCalled();
+    });
+
+    test('call static addPagination() method: { output: { replies }, limit, currentPage }', () => {
+      expect(StoryMock.addPagination).toHaveBeenCalledWith({ output: { replies }, limit, currentPage });
+    });
   });
 });
