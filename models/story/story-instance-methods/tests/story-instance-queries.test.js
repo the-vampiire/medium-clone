563556d0b995fb93c2a0a21e72063b4a18360476
The-Vampiire <vampiirecodes@gmail.com>
2019-02-09 22:42:17 -0500

    Story instance queries: ref getClappedReaders() logic, ref tests

--- models/story/story-instance-methods/tests/story-instance-queries.test.js
+++ models/story/story-instance-methods/tests/story-instance-queries.test.js
@@ -1,34 +1,67 @@
 const { getClapsCount, getClappedReaders, getReplies } = require('../story-instance-queries');
 
-const populaterMock = {
-  populate() { return this; },
-  execPopulate() { return new Promise(res => res(this)); },
-}
-
 describe('Story instance query methods', () => {
   test('getClapsCount(): returns the aggregate count of all claps from readers', async () => {
     const numClaps = 5;
     const countPerClap = 20;
     const claps = Array(numClaps).fill().map(() => ({ count: countPerClap }));
     // mock a Story that can populate its claps property
-    const storyMock = Object.assign({ claps, getClapsCount }, populaterMock);
+    const storyMock = {
+      claps,
+      getClapsCount,
+      populate: jest.fn(() => storyMock),
+      execPopulate: jest.fn(() => storyMock),
+    };
+    storyMock.execPopulate.mockImplementation(() => new Promise(res => res(storyMock)));
 
     const output = await storyMock.getClapsCount();
     expect(output).toBe(numClaps * countPerClap);
+
+    jest.clearAllMocks();
   });
+  
+  describe('getClappedReaders(): gets a paginated list of clapped readers', () => {
+    const limit = 17;
+    const currentPage = 2;
+    const query = { limit, currentPage };
 
-  test('getClappedReaders(): returns an Array [User] of all the clapped readers', async () => {
-    const numClaps = 5;
-    // mock a Clap that can populate its user property
-    const clapMock = (id) => Object.assign({ user: { id } }, populaterMock);
-    // assign the mapping index to the clap.user.id property for simple testing
-    const claps = Array(numClaps).fill().map((_, id) => clapMock(id));
-    // mock a Story that can populate its claps property
-    const storyMock = Object.assign({ claps, getClappedReaders }, populaterMock);
+    const readerMock = { toResponseShape: jest.fn(() => readerMock) };
+    const clapMock = {
+      reader: readerMock,
+      populate: jest.fn(() => clapMock),
+      execPopulate: jest.fn(() => clapMock),
+    };
 
-    const output = await storyMock.getClappedReaders();
-    expect(output.length).toBe(numClaps);
-    expect(output[0].id).toBe(0); // IDs are set by index when mapping the clap mocks
+    const StoryMock = { addPagination: jest.fn() };
+    const storyMock = {
+      claps: [clapMock],
+      getClappedReaders,
+      model: jest.fn(() => StoryMock),
+      populate: jest.fn(() => storyMock),
+      execPopulate: jest.fn(() => storyMock),
+    };
+
+    beforeAll(() => storyMock.getClappedReaders(query));
+    afterAll(() => jest.clearAllMocks());
+
+    test('populates the story claps: uses pagination, sort by count descending', () => {
+      const expectedPopulateOptions = {
+        path: 'claps',
+        options: { limit, skip: currentPage * limit, sort: { count: -1 } },
+      };
+
+      expect(storyMock.populate).toHaveBeenCalledWith(expectedPopulateOptions);
+    });
+
+    test('populates and calls toResponseShape() the reader of each clap', () => {
+      expect(clapMock.populate).toHaveBeenCalledWith('reader');
+      expect(readerMock.toResponseShape).toHaveBeenCalled();
+    });
+
+    test('calls the Story static addPagination() method: { output: { readers }, limit, currentPage }', () => {
+      const output = { readers: [readerMock] };
+      expect(StoryMock.addPagination).toHaveBeenCalledWith({ output, limit, currentPage });
+    });
   });
 
   describe('getReplies(): gets a paginated list of the story replies', () => {
@@ -51,12 +84,13 @@ describe('Story instance query methods', () => {
     const storyMock = {
       replies,
       getReplies,
+      model: jest.fn(() => StoryMock),
       populate: jest.fn(() => storyMock),
       execPopulate: jest.fn(() => storyMock),
-      model: jest.fn(() => StoryMock), // simulate Story model access
-    };
+    }
 
     beforeAll(() => storyMock.getReplies({ limit, currentPage }));
+    afterAll(() => jest.clearAllMocks());
 
     test('populates the replies: uses pagination, publishedAt desc sorting, and only published', () => {
       expect(storyMock.populate).toHaveBeenCalledWith(expectedPopulateOptions);
