e9a63ff85100d3e4f95335a244cc80c33ec952c2
The-Vampiire <vampiirecodes@gmail.com>
2019-02-10 20:15:16 -0500

    Clap: count min/max error messages, composite unique index reader-story

--- models/clap/clap.js
+++ models/clap/clap.js
@@ -8,12 +8,15 @@ const clapSchema = new mongoose.Schema({
   count: {
     type: Number,
     default: 1,
-    min: 1,
-    max: MAX_CLAP_COUNT,
+    min: [1, 'claps count must be at least 1'],
+    max: [MAX_CLAP_COUNT, `claps count can not exceed ${MAX_CLAP_COUNT}`],
   },
   reader: { type: mongoose.SchemaTypes.ObjectId, ref: 'users' },
   story: { type: mongoose.SchemaTypes.ObjectId, ref: 'stories' },
 }, { timestamps: true });
+
+// unique composite index reader-story, reader can create one clap per story
+clapSchema.index({ reader: 1, story: 1 }, { unique: true });
 
 // -- INSTANCE METHODS -- //
 clapSchema.methods.toResponseShape = toResponseShape;
@@ -25,3 +28,6 @@ module.exports = {
   Clap,
   MAX_CLAP_COUNT,
 };
+
+
+
