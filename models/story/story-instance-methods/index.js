1db639040a387626e5c28e17b05fc557e17b8a1e
The-Vampiire <vampiirecodes@gmail.com>
2019-02-10 23:51:15 -0500

    Story instance methods: remove deprecated publish() mutation

--- models/story/story-instance-methods/index.js
+++ models/story/story-instance-methods/index.js
@@ -1,9 +1,7 @@
 const queryMethods = require('./story-instance-queries');
 const shaperMethods = require('./story-instance-shapers');
-const mutationMethods = require('./story-instance-mutations');
 
 module.exports = {
   ...queryMethods,
   ...shaperMethods,
-  ...mutationMethods,
 };
