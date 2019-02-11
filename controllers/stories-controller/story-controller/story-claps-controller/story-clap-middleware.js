5d1ee9613f90331ae37a35361d4dc5a987781041
The-Vampiire <vampiirecodes@gmail.com>
2019-02-10 20:16:59 -0500

    Story-Clap MW: injectStoryClap() for /:usernameSlug and tests

---
+++ controllers/stories-controller/story-controller/story-claps-controller/story-clap-middleware.js
@@ -0,0 +1,13 @@
+const injectStoryClap = async (req, res, next) => {
+  const { pathUser, pathStory, models } = req;
+
+  const pathClap = await models.Clap.findOne({ reader: pathUser, story: pathStory });
+  if (!pathClap) return res.status(400).json({ error: 'clap not found' });
+  
+  req.pathClap = pathClap;
+  next();
+};
+
+module.exports = {
+  injectStoryClap,
+};
