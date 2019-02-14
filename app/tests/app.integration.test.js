8b333b7689cba09505a35652a641ff502ae13bc0
The-Vampiire <vampiirecodes@gmail.com>
2019-02-13 19:36:23 -0500

    Integration tests: get followers, following, get and delete follower

--- app/tests/app.integration.test.js
+++ app/tests/app.integration.test.js
@@ -13,14 +13,8 @@ const users = [vamp, witch];
 const { DOMAIN } = process.env;
 
 describe('App integration tests', () => {
-  beforeAll(async () => {
-    await dbConnect(mongoose);
-    process.env.SALT_ROUNDS = '1';
-  });
-  afterAll(async () => {
-    await teardown(mongoose, ['users', 'stories', 'claps']);
-    delete process.env.SALT_ROUNDS;
-  });
+  beforeAll(async () => dbConnect(mongoose));
+  afterAll(async () => teardown(mongoose, ['users', 'stories', 'claps']));
 
   describe('POST /users: registering new users', () => {
     users.forEach(
@@ -63,6 +57,58 @@ describe('App integration tests', () => {
         .set('Authorization', `Bearer ${vamp.token}`)
         .expect(201)
         .expect('Location', `${DOMAIN}/users/@witch/followers/@vamp`);
+    });
+  });
+  
+  describe('GET /users/@username/followers: retrieve the users followers', () => {
+    test('witch followers includes vamp', async () => {
+      const res = await request(app).get('/users/@witch/followers');
+      const { body } = res;
+      expect(body.followers).toBeDefined();
+      expect(body.followers.length).toBe(1);
+      expect(body.followers[0].username).toBe('vamp');
+    });
+
+    test('vamp followers list is empty', async () => {
+      const res = await request(app).get('/users/@vamp/followers');
+      const { body } = res;
+      expect(body.followers).toBeDefined();
+      expect(body.followers.length).toBe(0);
+    });
+  });
+
+  describe('GET /users/@username/following: retrieve the list of users the user is following', () => {
+    test('vamp followed users list includes witch', async () => {
+      const res = await request(app).get('/users/@vamp/following');
+      const { body } = res;
+      expect(body.followed_users).toBeDefined();
+      expect(body.followed_users.length).toBe(1);
+      expect(body.followed_users[0].username).toBe('witch');
+    });
+
+    test('witch followed users list is empty', async () => {
+      const res = await request(app).get('/users/@witch/following');
+      const { body } = res;
+      expect(body.followed_users).toBeDefined();
+      expect(body.followed_users.length).toBe(0);
+    });
+  });
+
+  describe('GET /users/@username/followers/@followerUsername: confirm if a user (follower) is following the path user', () => {
+    test('vamp is confirmed to be following witch: 204 no-content response', () => {
+      return request(app).get('/users/@witch/followers/@vamp').expect(204);
+    });
+
+    test('witch is confirmed to not be following vamp: 404 response', () => {
+      return request(app).get('/users/@vamp/followers/@witch').expect(404);
+    });
+  });
+
+  describe('DELETE /users/@username/followers/@followerUsername: authed user (follower) unfollow path user', () => {
+    test('vamp can unfollow witch: 204 no-content response', () => {
+      return request(app).delete('/users/@witch/followers/@vamp')
+      .set('Authorization', `Bearer ${vamp.token}`)
+      .expect(204);
     });
   });
 });
