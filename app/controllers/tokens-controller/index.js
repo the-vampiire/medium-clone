f6059622d2962187d29a3bd07e379b6ef7365a53
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-01 17:28:01 -0500

    Tokens Controller: config route handlers and MW

--- app/controllers/tokens-controller/index.js
+++ app/controllers/tokens-controller/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const createTokenHandler = require('./create-token-handler');
-const { verifyPayload, authenticateRequest } = require('./token-middleware');
 
+const {
+  verifyPayload,
+  authenticateRequest,
+  validateRefreshToken,
+} = require('./tokens-middleware');
+const {
+  createAccessTokenHandler,
+  createRefreshTokenHandler,
+  revokeRefreshTokenHandler,
+} = require('./tokens-route-handlers');
+
+// controls: /tokens/
 const TokensController = express.Router();
 
-TokensController.post('/', verifyPayload, authenticateRequest, createTokenHandler);
+TokensController.post('/', verifyPayload, authenticateRequest, createRefreshTokenHandler);
+
+TokensController.delete('/', validateRefreshToken, revokeRefreshTokenHandler);
+
+TokensController.get('/access_token', validateRefreshToken, createAccessTokenHandler);
 
 module.exports = TokensController;
