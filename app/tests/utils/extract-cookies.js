ed6c5c984f526965dc02750ddf97246ba0aa90fe
Vamp <vampiirecodes@gmail.com>
2019-07-04 01:41:00 -0400

    test utils: extractCookies and tests (for integration tests)

---
+++ app/tests/utils/extract-cookies.js
@@ -0,0 +1,26 @@
+const shapeFlags = flags =>
+  flags.reduce((shapedFlags, flag) => {
+    const [flagName, rawValue] = flag.split("=");
+    // edge case where a cookie has a single flag and "; " split results in trailing ";"
+    const value = rawValue ? rawValue.replace(";", "") : true;
+    return { ...shapedFlags, [flagName]: value };
+  }, {});
+
+const extractCookies = headers => {
+  const cookies = headers["set-cookie"]; // Cookie[]
+
+  return cookies.reduce((shapedCookies, cookieString) => {
+    const [rawCookie, ...flags] = cookieString.split("; ");
+    const [cookieName, value] = rawCookie.split("=");
+    
+    return {
+      ...shapedCookies,
+      [cookieName]: { value, flags: shapeFlags(flags) },
+    };
+  }, {});
+};
+
+module.exports = {
+  shapeFlags,
+  extractCookies,
+};
